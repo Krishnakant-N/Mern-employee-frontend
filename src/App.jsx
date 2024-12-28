4ca3a83aa18df32f79170ad2db0d9e98ba71779f
Krishnakant-N <krishnakant.13.niwaria@gmail.com>
2024-12-28 14:29:46 +0530

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import LoginPage from './components/LoginPage';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
